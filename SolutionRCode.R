170c413b2552736dfb84e81950331b997862adfe
Juan Arturo Nolazco-Flores <jnolazco@tec.mx>
2020-07-22 01:14:53 -0500

    These are the function makeCacheMatrix and cacheSolve required for Assignment 2 of the course "R Programming".

---
+++ SolutionRCode.R
@@ -0,0 +1,43 @@
+############
+# This function creates a special "matrix" object that can cache its inverse, which is really a list containing 
+# a function to
+# 1. set the value of the vector
+# 2. get the value of the vector
+# 3. set the value of the inverse matrix
+# 4. get the value of the inverse matrix
+makeCacheMatrix <- function(x = matrix()) {
+      invMat <- NULL
+      set <- function(y) {                      # 1. set the value of the matrix
+            x <<- y
+            invMat <<- NULL
+      }
+      get <- function() x                       # 2. get the value of the matrix
+      setinverse <- function(inverseMatrix) invMat <<- inverse # 3. set the value of the matrix in the cache
+      getinverse <- function() invMat                          # 4. get the value of the matrix
+      list( set = set,
+            get = get,
+            setinverse = setinverse,
+            getinverse = getinverse)
+}
+
+############
+# This function computes the inverse of the special "matrix" returned by makeCacheMatrix 
+# above. If the inverse has already been calculated (and the matrix has not changed), 
+# then the cachesolve should retrieve the inverse from the cache.
+cacheSolve <- function(x, ...) {
+      ## Return a matrix that is the inverse of 'x'
+      invMat <- x$getinverse()
+      if(!is.null(invMat)) {                          # if inverse matrix has been already calculated
+            message("getting cached data")
+            return(invMat)                            # get the inverse matrix from the cache.
+      }
+      data <- x$get()                                 # if not, it gets the data
+      invMat <- solve(data, ...)                      # calculate the inverse matrix of the data.
+      x$setinverse(invMat)                            # set the value of the inverse matrix in the cache.
+      i
+} 
+
+
+
+
+
